e3ca7c77b5d5bde11b6842af2d1009a7873f4009
刘立俊 <645604899@qq.com>
2020-06-15 00:03:32 +0800

    配置路由

--- src/main/java/com/cloud/personal/zuulserver/ZuulServerApplication.java
+++ src/main/java/com/cloud/personal/zuulserver/ZuulServerApplication.java
@@ -2,8 +2,12 @@ package com.cloud.personal.zuulserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
 public class ZuulServerApplication {
 
     public static void main(String[] args) {
